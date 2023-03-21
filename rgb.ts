b8b1cfbf033605092f27be6288ab3c73012e6132
Wai Weng <waiweng@cytron.com.my>
2023-03-21 18:31:02 +0800

    Added blocks for external RGB LED

--- rgb.ts
+++ rgb.ts
@@ -16,13 +16,15 @@ const RGB_LED_LENGTH = 2;
 
 
 namespace motionbit {
-    // Colors array for each pixel.
+    // Colors array for each pixel.\
+    let extColorsArray: number[] = [];
     let colorsArray: number[] = [];
     for (let i = 0; i < RGB_LED_LENGTH; i++) {
         colorsArray.push(0);
     }
 
     // Create a Neo Pixel object for RGB LED.
+    let extRgbLed: motionbit.Strip;
     let rgbLed = motionbit.create(RGB_LED_PIN, RGB_LED_LENGTH, MotionBitRgbMode.RGB);
     rgbLed.clear();
 
@@ -141,6 +143,217 @@ namespace motionbit {
     //% blue.min=0 blue.max=255
     export function rgb(red: number, green: number, blue: number): number {
         return ((red & 0xFF) << 16) | ((green & 0xFF) << 8) | (blue & 0xFF);
+    }
+
+
+
+    /**
+     * Set the length for external RGB pixels.
+     * @param length Number of external RGB pixels. eg: 8
+     */
+    //% subcategory="External RGB LED"
+    //% weight=30
+    //% blockGap=40
+    //% blockId="motionbit_init_ext_rgb"
+    //% block="set external RGB pixels length to %length"
+    export function initExternalRgb(length: number): void {
+        if (length == 0) return;
+
+        // Create the colors array.
+        for (let i = 0; i < length; i++) {
+            extColorsArray.push(0);
+        }
+
+        // Create a Neo Pixel object for RGB LED.
+        extRgbLed = rgbLed.range(2, length);
+        extRgbLed.clear();
+
+        // Reduce the default brightness.
+        extRgbLed.setBrightness(25);
+    }
+
+
+
+    /**
+     * Turn off all External RGB pixels.
+     */
+    //% subcategory="External RGB LED"
+    //% weight=29
+    //% blockGap=8
+    //% blockId="motionbit_clear_all_ext_rgb_pixels"
+    //% block="clear all external RGB pixels"
+    export function clearAllExternalRgbPixels(): void {
+        if (extRgbLed == null) return;
+
+        for (let i = 0; i < extColorsArray.length; i++) {
+            extColorsArray[i] = 0;
+        }
+
+        extRgbLed.clear();
+        extRgbLed.show();
+        basic.pause(0);
+    }
+
+
+    /**
+     * Set the brightness of the external RGB pixels (0-255).
+     * @param brightness Pixel brightness. eg: 25
+     */
+    //% subcategory="External RGB LED"
+    //% weight=28
+    //% blockGap=40
+    //% blockId="motionbit_set_ext_rgb_brightness"
+    //% block="set external RGB pixels brightness to %brightness"
+    //% brightness.min=0 brightness.max=255
+    export function setExternalRgbBrightness(brightness: number): void {
+        if (extRgbLed == null) return;
+
+        extRgbLed.setBrightness(brightness);
+
+        // Restore the original color.
+        for (let i = 0; i < extColorsArray.length; i++) {
+            extRgbLed.setPixelColor(i, colorsArray[i]);
+        }
+        extRgbLed.show();
+        basic.pause(0);
+    }
+
+
+    /**
+     * Show the same color on all external RGB pixels. 
+     * @param color RGB color of the pixel. eg: 0xff0000
+     */
+    //% subcategory="External RGB LED"
+    //% weight=27
+    //% blockGap=8
+    //% blockId="motionbit_set_all_ext_rgb_pixels_color"
+    //% block="set all external RGB pixels to %color"
+    //% color.shadow="colorNumberPicker"
+    export function setAllExternalRgbPixelsColor(color: number): void {
+        if (extRgbLed == null) return;
+
+        for (let i = 0; i < extColorsArray.length; i++) {
+            extColorsArray[i] = color;
+        }
+        extRgbLed.showColor(color);
+        basic.pause(0);
+    }
+
+
+    /**
+     * Show color on individual external RGB pixels.
+     * @param pixel The pixel number we want to change the color.
+     * @param color RGB color of the pixel. eg: 0xff0000
+     */
+    //% subcategory="External RGB LED"
+    //% weight=26
+    //% blockGap=40
+    //% blockId="motionbit_set_ext_rgb_pixel_color"
+    //% block="set external RGB pixel %pixel to %color"
+    //% color.shadow="colorNumberPicker"
+    export function setExternalRgbPixelColor(pixel: number, color: number): void {
+        if (extRgbLed == null) return;
+
+        extColorsArray[pixel] = color;
+        extRgbLed.setPixelColor(pixel, color);
+        extRgbLed.show();
+        basic.pause(0);
+    }
+
+
+
+    /**
+     * Shift the color of external RGB pixels.
+     * @param offset Number of pixels to shift. eg: 1
+     */
+    //% subcategory="External RGB LED"
+    //% weight=25
+    //% blockGap=8
+    //% blockId="motionbit_shift_ext_rgb_pixels"
+    //% block="shift external RGB pixels color by %offset"
+    export function shiftExternalRgbPixels(offset: number): void {
+        if (extRgbLed == null) return;
+
+        // Do nothing if offset is 0.
+        if (offset == 0) return;
+
+        // Shift forward.
+        else if (offset > 0) {
+            while (offset-- > 0) {
+                for (let i = extColorsArray.length - 1; i > 0; i--) {
+                    extColorsArray[i] = extColorsArray[i - 1];
+                }
+                extColorsArray[0] = 0;
+            }
+        }
+
+        // Shift backward.
+        else {
+            offset = -offset;
+            while (offset-- > 0) {
+                for (let i = 0; i < extColorsArray.length - 1; i++) {
+                    extColorsArray[i] = extColorsArray[i + 1];
+                }
+                extColorsArray[extColorsArray.length - 1] = 0;
+            }
+        }
+
+
+        // Show the new color.
+        for (let i = 0; i < extColorsArray.length; i++) {
+            extRgbLed.setPixelColor(i, extColorsArray[i]);
+        }
+        extRgbLed.show();
+        basic.pause(0);
+    }
+
+
+
+    /**
+     * Rotate the color of RGB pixels(-3 to 3).
+     * @param offset Number of pixels to rotate. eg: 1
+     */
+    //% subcategory="External RGB LED"
+    //% weight=24
+    //% blockGap=40
+    //% blockId="motionbit_rotate_ext_rgb_pixels"
+    //% block="rotate external RGB pixels color by %offset"
+    export function rotateExternalRgbPixels(offset: number): void {
+        if (extRgbLed == null) return;
+
+        // Do nothing if offset is 0.
+        if (offset == 0) return;
+
+        // Rotate forward.
+        else if (offset > 0) {
+            while (offset-- > 0) {
+                let lastLed = extColorsArray[extColorsArray.length - 1];
+                for (let i = extColorsArray.length - 1; i > 0; i--) {
+                    extColorsArray[i] = extColorsArray[i - 1];
+                }
+                extColorsArray[0] = lastLed;
+            }
+        }
+
+        // Rotate backward.
+        else {
+            offset = -offset;
+            while (offset-- > 0) {
+                let lastLed = extColorsArray[0];
+                for (let i = 0; i < extColorsArray.length - 1; i++) {
+                    extColorsArray[i] = extColorsArray[i + 1];
+                }
+                extColorsArray[extColorsArray.length - 1] = lastLed;
+            }
+        }
+
+
+        // Show the new color.
+        for (let i = 0; i < extColorsArray.length; i++) {
+            extRgbLed.setPixelColor(i, extColorsArray[i]);
+        }
+        extRgbLed.show();
+        basic.pause(0);
     }
 
 }
